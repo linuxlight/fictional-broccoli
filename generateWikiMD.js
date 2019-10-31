1d479c450145cd20120cd4927369b3ffcb26ac96
linuxlight <linuxlight@users.noreply.github.com>
2019-10-31 22:08:59 +0900

    try to read content.html when node runs

--- generateWikiMD.js
+++ generateWikiMD.js
@@ -3,7 +3,13 @@ var marked = require('marked');
 var fs = require('fs');
 //markdown 파일을 읽어들입니다.
 var wikiMD = fs.readFileSync('wikiMD.md', 'utf-8');
+
+var html = fs.readFileSync('content.html', 'utf-8');
+
 //markdown을 html로 변환합니다.
 var htmlwikiMD = marked(wikiMD);
+
+html.getElementById('content').innerHTML = htmlwikiMD;
+
 //변환된 양식을 파일로 저장합니다.
 fs.writeFileSync('./wikiMD.html', htmlwikiMD);
