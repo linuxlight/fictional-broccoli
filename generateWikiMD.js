fd2ea7ee8ed53c95fe8727c595dd0fa03424f202
linuxlight <lnxlht4j@gmail.com>
2019-11-02 16:48:09 +0900

    md update will be done from node

--- generateWikiMD.js
+++ generateWikiMD.js
@@ -3,13 +3,9 @@ var marked = require('marked');
 var fs = require('fs');
 //markdown 파일을 읽어들입니다.
 var wikiMD = fs.readFileSync('wikiMD.md', 'utf-8');
-
+//변경할 html파일을 읽어옵니다.
 var html = fs.readFileSync('content.html', 'utf-8');
-
-//markdown을 html로 변환합니다.
-var htmlwikiMD = marked(wikiMD);
-
-html.getElementById('content').innerHTML = htmlwikiMD;
-
+//html파일을 변경합니다.
+html = html.replace(/<div id="content">(.*)<\/div>/, '<div id="content">'+marked(wikiMD)+'<\/div>');
 //변환된 양식을 파일로 저장합니다.
-fs.writeFileSync('./wikiMD.html', htmlwikiMD);
+fs.writeFileSync('./content.html', html);
